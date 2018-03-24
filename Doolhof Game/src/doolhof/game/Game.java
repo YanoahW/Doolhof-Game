0192d293452fca68f4186aac941eb48bbb864749
yanoah98 <yanoahw@gmail.com>
2018-03-24 15:31:25 +0100

    Headers 2.0

--- Doolhof Game/src/doolhof/game/Game.java
+++ Doolhof Game/src/doolhof/game/Game.java
@@ -1,11 +1,4 @@
 package doolhof.game;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Yanoah Wiersma & Remon Turk
