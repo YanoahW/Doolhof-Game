51aebaf69e369ae8cc5747604b32776afcac4643
remonturk <35971818+remonturk@users.noreply.github.com>
2018-03-24 15:10:33 +0100

    Fix 4

--- Doolhof Game/src/doolhof/game/Game.java
+++ Doolhof Game/src/doolhof/game/Game.java
@@ -14,6 +14,7 @@ package doolhof.game;
 public class Game {
     public static void main(String[] args) {
         System.out.println("doe is ff wat leuks");
+        System.out.println("niet hetzelfde");
     }
     
 
