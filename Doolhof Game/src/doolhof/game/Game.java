4260fe6955dc1686b4d02074004cbbbf0b20c35c
yanoah98 <yanoahw@gmail.com>
2018-03-24 15:09:47 +0100

    Fix 3

--- Doolhof Game/src/doolhof/game/Game.java
+++ Doolhof Game/src/doolhof/game/Game.java
@@ -14,6 +14,14 @@ package doolhof.game;
 public class Game {
     public static void main(String[] args) {
         System.out.println("doe is ff wat leuks");
+        System.out.println("doe is ff wat leuks");
+        System.out.println("doe is ff wat leuks");
+        System.out.println("doe is ff wat leuks");
+        System.out.println("doe is ff wat leuks");
+        System.out.println("doe is ff wat leuks");
+        System.out.println("doe is ff wat leuks");
+        System.out.println("doe is ff wat leuks");
+        System.out.println("doe is ff wat leuks");
     }
     
 
