88f3d7ed84e2b2c4738bfefbf207cde02be0029c
yanoah98 <yanoahw@gmail.com>
2018-03-30 14:14:30 +0200

    Fix

--- Doolhof Game/src/doolhof/game/DoolhofGame.java
+++ Doolhof Game/src/doolhof/game/DoolhofGame.java
@@ -12,16 +12,10 @@ import java.io.IOException;
 public class DoolhofGame {
     public static void main (String[] args) throws IOException
     {
-<<<<<<< HEAD
-        System.out.println(100 - 030);
         Game game = new Game();
         Field field = new Field(10,10);
         field.setUpField("src/doolhof/game/data/standardField.txt");
         game.setField(field);
-=======
-        Game game = new Game("src/doolhof/game/data/standardField.txt");
-        
->>>>>>> 044fbe509d5729545909b2a870934ca7af556fed
     }
     
 }
